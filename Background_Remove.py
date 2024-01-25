fd2a51e8a8e6ee0f7943e8e1808eb0c2c1103fe8
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-01-25 16:44:57 +0900

    background : codeadd

--- Background_Remove.py
+++ Background_Remove.py
@@ -1,6 +1,10 @@
 from rembg import remove
 from PIL import Image
 
+TOP_img_dir = '/Users/seunghunjang/Desktop/Coordination/Coordination/train/TOP'
+BOTTOM_img_dir = '/Users/seunghunjang/Desktop/Coordination/Coordination/train/BOTTOM'
+
+
 # 이미지 배경 제거
 input = Image.open('/Users/seunghunjang/Desktop/MNIST_Test/Coordination/train/TOP/TOP_4.jpg') # load image
 output = remove(input) # remove background
