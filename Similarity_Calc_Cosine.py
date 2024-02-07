f814b7fed3efaa5fdf7ded497f71d75c8caf50b3
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-07 22:40:52 +0900

    feat : Model train epoch 30 -> 50 fix

--- Similarity_Calc_Cosine.py
+++ Similarity_Calc_Cosine.py
@@ -11,8 +11,8 @@ model = tf.keras.models.load_model('Model.h5')
 
 
 # 이미지 불러오기 및 전처리
-image_path_1 = 'path/to/image.PNG'
-image_path_2 = 'path/to/image.PNG'
+image_path_1 = '/Users/seunghunjang/Desktop/Coordination_CNN/Back_remove_Imgdir/TOP_4_B.PNG'
+image_path_2 = '/Users/seunghunjang/Desktop/Coordination_CNN/Back_remove_Imgdir/TOP_2_B.PNG'
 
 img1 = tf.keras.preprocessing.image.load_img(image_path_1, color_mode='grayscale',target_size=(28, 28))
 img1 = tf.keras.preprocessing.image.img_to_array(img1)
